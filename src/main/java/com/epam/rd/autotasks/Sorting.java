65070b8c269ac0bbdb4ec1341e960d27b0b2636a
Gbakradze94 <bakradzegeorge17@gmail.com>
2021-07-15 14:11:03 +0000

    Update Sorting.java

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,22 @@
 package com.epam.rd.autotasks;
 
 public class Sorting {
-    public void sort(int[] array){
-        throw new UnsupportedOperationException();
+   public void sort(int[] array) throws IllegalArgumentException {
+        for (int i = 0; i < array.length - 1; i++) {
+            int temp;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    // swap elements
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+        }
+        for (int i: array
+             ) {
+            System.out.println(i);
+        }
     }
 }
